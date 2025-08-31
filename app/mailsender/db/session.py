837b1246d78de798540e7eae150e54d7f81573e6
Mario Alemi <mario.alemi@gmail.com>
2025-08-31 19:33:48 +0200

    Configure SQLite pragmas

--- app/mailsender/db/session.py
+++ app/mailsender/db/session.py
@@ -1,9 +1,21 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 
 from ..config.settings import settings
 
 engine = create_engine(
-    settings.database_url, connect_args={"check_same_thread": False}, echo=False, future=True
+    settings.database_url,
+    connect_args={"check_same_thread": False, "timeout": 30},
+    echo=False,
+    future=True,
 )
+
+
+@event.listens_for(engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA journal_mode=WAL")
+    cursor.execute("PRAGMA busy_timeout = 30000")
+    cursor.close()
+
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
